6c732360298aaf7ba7e2e7ddf06bd2907badc360
AndrijaAda99 <andrija.ada99@gmail.com>
2023-05-15 00:11:24 +0200

    Add: testy testy-hunit testy-quickcheck packages

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "All tests"
+  [ unitTests 
+  , propertyTests
+  ]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+    [ testCase "2+2=4" $
+        2+2 @?= 4
+    ]
+
+propertyTests :: TestTree
+propertyTests = testGroup "Property tests"
+    [ testProperty "even + odd = odd" propertyEvenOdd
+    ]
+
+propertyEvenOdd :: Int -> Int -> Property
+propertyEvenOdd x y = even x && odd y ==> odd (x + y)
