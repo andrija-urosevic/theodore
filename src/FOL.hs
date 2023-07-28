bb1872946a560c0d0b6680b546b7c7609c44ae51
AndrijaAda99 <andrija.ada99@gmail.com>
2023-07-28 17:16:54 +0200

    Add: FOL Formula semantics

--- src/FOL.hs
+++ src/FOL.hs
@@ -1,20 +1,21 @@
-module FOL 
+module FOL
     ( Term ( Var, Fun )
     , Formula ( Rel, Top, Bot, Neg, Conj, Disj
               , Impl, Eqiv, Exis, Alls )
     , Assigment
     , FnInterp
     , RelInterp
+    , Model ( Model )
     , mkConstTerm
-    , mkConstRel
     , substTerm
     , evalTerm
+    , evalFormula
     ) where
 
 import qualified Data.List as List
 import qualified Data.Map as Map
 
-data Term 
+data Term
     = Var String
     | Fun String [Term]
     deriving (Eq, Ord)
@@ -44,19 +45,12 @@ data Model a =
 instance Show Term where
     show (Var x)    = x
     show (Fun f []) = f
-    show (Fun f ts) = f 
-                   ++ "(" 
-                   ++ (List.intercalate ", " . map show) ts 
-                   ++ ")"
+    show (Fun f ts) = f ++ "(" ++ (List.intercalate ", " . map show) ts ++ ")"
 
 instance Show Formula where
     show Top            = "⊤"
     show Bot            = "⊥"
-    show (Rel r [])     = r
-    show (Rel r ts)     = r
-                       ++ "("
-                       ++ (List.intercalate ", " . map show) ts
-                       ++ ")"
+    show (Rel r ts)     = r ++ "(" ++ (List.intercalate ", " . map show) ts ++ ")"
     show (Neg f)        = "(¬ " ++ show f ++ ")"
     show (Conj f1 f2)   = "(" ++ show f1 ++ " ∧ " ++ show f2 ++ ")"
     show (Disj f1 f2)   = "(" ++ show f1 ++ " ∨ " ++ show f2 ++ ")"
@@ -68,16 +62,28 @@ instance Show Formula where
 mkConstTerm :: String -> Term
 mkConstTerm c = Fun c []
 
-mkConstRel :: String -> Formula
-mkConstRel c = Rel c []
-
 substTerm :: Assigment Term -> Term -> Term
 substTerm sigma (Var p)     = Map.findWithDefault (Var p) p sigma
 substTerm sigma (Fun f ts)  = Fun f (map (substTerm sigma) ts)
 
 evalTerm :: FnInterp a -> Assigment a -> Term -> a
-evalTerm fn sigma (Var p)       = sigma Map.! p
-evalTerm fn sigma (Fun f ts)    = fn Map.! f 
-                                $ map (evalTerm fn sigma) 
+evalTerm _  sigma (Var p)       = sigma Map.! p
+evalTerm fn sigma (Fun f ts)    = fn Map.! f
+                                $ map (evalTerm fn sigma)
                                 $ ts
+
+evalFormula :: Model a -> Assigment a -> Formula -> Bool
+evalFormula _     _     Top             = True
+evalFormula _     _     Bot             = False
+evalFormula model sigma (Rel r ts)      = (rel model) Map.! r
+                                        $ map (evalTerm (fn model) sigma) ts
+evalFormula model sigma (Neg f)         = not (evalFormula model sigma f)
+evalFormula model sigma (Conj f1 f2)    = (evalFormula model sigma f1) && (evalFormula model sigma f2)
+evalFormula model sigma (Disj f1 f2)    = (evalFormula model sigma f1) || (evalFormula model sigma f2)
+evalFormula model sigma (Impl f1 f2)    = not (evalFormula model sigma f1) || (evalFormula model sigma f2)
+evalFormula model sigma (Eqiv f1 f2)    = not (evalFormula model sigma f1) || (evalFormula model sigma f2) && (evalFormula model sigma f1) || not (evalFormula model sigma f2)
+evalFormula model sigma (Alls x f)      = all (\val -> evalFormula model (Map.insert x val sigma) f)
+                                        $ univ model
+evalFormula model sigma (Exis x f)      = any (\val -> evalFormula model (Map.insert x val sigma) f)
+                                        $ univ model
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -81,7 +81,11 @@ bitArithAssigment = Map.fromList
     ]
 
 bitArithTerm :: Term
-bitArithTerm = FOL.Fun "+" [FOL.Fun "+" [FOL.Var "p", mkConstTerm "0"], FOL.Fun "*" [FOL.Var "p", FOL.Var "q"]]
+bitArithTerm =
+    FOL.Fun "+"
+    [ FOL.Fun "+" [FOL.Var "p", mkConstTerm "0"]
+    , FOL.Fun "*" [FOL.Var "p", FOL.Var "q"]
+    ]
 
 f1 :: Formula
 f1 = FOL.Rel "even" [FOL.Var "x"]
@@ -90,14 +94,157 @@ f2 :: Formula
 f2 = FOL.Rel "odd" [FOL.Var "x"]
 
 f3 :: Formula
-f3 = FOL.Alls "x" (FOL.Impl (FOL.Rel "even" [FOL.Var "x"]) (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "zero"]))
+f3 = FOL.Alls "x" (FOL.Impl
+        (FOL.Rel "even" [FOL.Var "x"])
+        (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "zero"])
+    )
 
 f4 :: Formula
-f4 = FOL.Alls "x" (FOL.Impl (FOL.Neg (FOL.Rel "even" [FOL.Var "x"])) (FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"])))
+f4 = FOL.Alls "x" (FOL.Impl
+        (FOL.Neg (FOL.Rel "even" [FOL.Var "x"]))
+        (FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"]))
+    )
+
+babyArithModel :: Model Int
+babyArithModel = Model
+    [0..9]
+    ( Map.fromList
+    [ ("+",     \args -> args !! 0 + args !! 1 `mod` 10)
+    , ("*",     \args -> args !! 0 * args !! 1 `mod` 10) 
+    , ("zero",  \_ -> 0)
+    , ("one",   \_ -> 1)
+    , ("two",   \_ -> 2)
+    , ("three", \_ -> 3)
+    , ("four",  \_ -> 4)
+    , ("five",  \_ -> 5)
+    , ("six",   \_ -> 6)
+    , ("seven", \_ -> 7)
+    , ("eight", \_ -> 8)
+    , ("nine",  \_ -> 9)
+    ])
+    ( Map.fromList
+    [ ("eq",    \args -> args !! 0 == args !! 1)
+    , ("lt",    \args -> args !! 0 < args !! 1)
+    , ("even",  \args -> even $ args !! 0)
+    ])
+
+-- even(x)
+babyFormula1 :: Formula
+babyFormula1 = FOL.Rel "even" [FOL.Var "x"]
+
+-- ∃y. lt(x, y)
+babyFormula2 :: Formula
+babyFormula2 = FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"])
+
+-- even(two)
+babyFormula3 :: Formula
+babyFormula3 = FOL.Rel "even" [mkConstTerm "two"]
+
+-- even(three)
+babyFormula4 :: Formula
+babyFormula4 = FOL.Rel "even" [mkConstTerm "three"]
+
+-- ∃x. even(%x)
+babyFormula5 :: Formula
+babyFormula5 = FOL.Exis "x" (FOL.Rel "even" [FOL.Var "x"])
+
+-- ∀x. even(%x)
+babyFormula6 :: Formula
+babyFormula6 = FOL.Alls "x" (FOL.Rel "even" [FOL.Var "x"])
+
+-- ∃x. lt(x, two) ∧ even(x)
+babyFormula7 :: Formula
+babyFormula7 =
+    FOL.Exis "x" (FOL.Conj
+        (FOL.Rel "lt" [FOL.Var "x", mkConstTerm "two"])
+        (FOL.Rel "even" [FOL.Var "x"])
+    )
+
+-- ∃x. ∃y. lt(x, y) ∧ lt(y, two) ∧ even(x) ∧ even(y)
+babyFormula8 :: Formula
+babyFormula8 =
+    FOL.Exis "x" (FOL.Exis "y" (FOL.Conj
+        (FOL.Conj
+            (FOL.Conj
+                (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"])
+                (FOL.Rel "lt" [FOL.Var "y", mkConstTerm "two"])
+            )
+            (FOL.Rel "even" [FOL.Var "x"])
+        )
+        (FOL.Rel "even" [FOL.Var "y"])
+    ))
+
+-- ∀x. even(x) ∧ lt(x, two) → x = zero
+babyFormula9 :: Formula
+babyFormula9 =
+    FOL.Alls "x" (FOL.Impl
+        (FOL.Conj
+            (FOL.Rel "even" [FOL.Var "x"])
+            (FOL.Rel "lt" [FOL.Var "x", mkConstTerm "two"])
+        )
+        (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "zero"])
+    )
+
+-- ∀x. even(x) ∧ lt(x,three) → x = zero
+babyFormula10 :: Formula
+babyFormula10 =
+    FOL.Alls "x" (FOL.Impl
+        (FOL.Conj
+            (FOL.Rel "even" [FOL.Var "x"])
+            (FOL.Rel "lt" [FOL.Var "x", mkConstTerm "three"])
+        )
+        (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "zero"])
+    )
+
+-- ∀x. even(x) ∧ lt(x,three) → x = zero ∨ x = two
+babyFormula11 :: Formula
+babyFormula11 =
+    FOL.Alls "x" (FOL.Impl
+        (FOL.Conj
+            (FOL.Rel "even" [FOL.Var "x"])
+            (FOL.Rel "lt" [FOL.Var "x", mkConstTerm "three"])
+        )
+        (FOL.Disj
+            (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "zero"])
+            (FOL.Rel "eq" [FOL.Var "x", mkConstTerm "two"])
+        )
+    )
+
+-- ∀x. ∃y. lt(x, y)
+babyFormula12 :: Formula
+babyFormula12 =
+    FOL.Alls "x" (FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"]))
+
+-- ∀x. even(x) → ∃y. lt(x, y)
+babyFormula13 :: Formula
+babyFormula13 =
+    FOL.Alls "x" (FOL.Impl
+        (FOL.Rel "even" [FOL.Var "x"])
+        (FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"]))
+    )
+
+-- ∀x. ¬ even(x) → ∃y. lt(x, y)
+babyFormula14 :: Formula
+babyFormula14 =
+    FOL.Alls "x" (FOL.Impl
+        (FOL.Neg (FOL.Rel "even" [FOL.Var "x"]))
+        (FOL.Exis "y" (FOL.Rel "lt" [FOL.Var "x", FOL.Var "y"]))
+    )
+
+trivAssigment :: Assigment Int
+trivAssigment = Map.fromList []
+
+-- x->5
+babyAssigment1 :: Assigment Int
+babyAssigment1 = Map.fromList [ ("x", 5) ]
+
+-- x->6
+babyAssigment2 :: Assigment Int
+babyAssigment2 = Map.fromList [ ("x", 6) ]
 
 tests :: TestTree
 tests = testGroup "All tests"
-  [ unitTests 
+  [ unitTests
   , propertyTests
   ]
 
@@ -125,6 +272,38 @@ unitTests = testGroup "Unit tests"
         negateLit (negateLit negr) @?= negr
     , testCase "bitArith" $
         evalTerm binAirthInterp bitArithAssigment bitArithTerm @?= True 
+    , testCase "eval even(x); x->5" $
+        evalFormula babyArithModel babyAssigment1 babyFormula1 @?= False
+    , testCase "eval even(x); x->5" $
+        evalFormula babyArithModel babyAssigment2 babyFormula1 @?= True
+    , testCase "eval ∃y. lt(x, y); x->5" $
+        evalFormula babyArithModel babyAssigment1 babyFormula2 @?= True
+    , testCase "eval ∃y. lt(x, y); x->5" $
+        evalFormula babyArithModel babyAssigment2 babyFormula2 @?= True
+    , testCase ("eval " ++ show babyFormula3 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula3 @?= True
+    , testCase ("eval " ++ show babyFormula4 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula4 @?= False
+    , testCase ("eval " ++ show babyFormula5 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula5 @?= True
+    , testCase ("eval " ++ show babyFormula6 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula6 @?= False
+    , testCase ("eval " ++ show babyFormula7 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula7 @?= True
+    , testCase ("eval " ++ show babyFormula8 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula8 @?= False
+    , testCase ("eval " ++ show babyFormula9 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula9 @?= True
+    , testCase ("eval " ++ show babyFormula10 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula10 @?= False
+    , testCase ("eval " ++ show babyFormula11 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula11 @?= True
+    , testCase ("eval " ++ show babyFormula12 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula12 @?= False
+    , testCase ("eval " ++ show babyFormula13 ++ " = True") $
+        evalFormula babyArithModel trivAssigment babyFormula13 @?= True
+    , testCase ("eval " ++ show babyFormula14 ++ " = False") $
+        evalFormula babyArithModel trivAssigment babyFormula14 @?= False
     ]
 
 propertyTests :: TestTree
